c5fd8ff95d303b8531f9d66242e959bbced1b98f
Mathieu <doniermathieu@gmail.com>
2019-03-09 08:51:30 +0100

    Create views and routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def films
+  end
+
+  def préinscription
+  end
+
+  def actualité
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  get 'films', to: 'home#films'
+  get '/préinscription', to: 'home#préinscription'
+  get '/actualité', to: 'home#actualité'
 
   resources :reservations
   get 'reservation/confirmation', to: 'reservations#show', as: :reserv_confirm
